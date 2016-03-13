93b96c8db6331927bb0c2fbab2f799b74ba4ba76
Alex Tran <tranad2@uci.edu>
2016-03-12 16:59:34 -0800

    Added logic for pipeEM

--- design/pipeEM.vhd
+++ design/pipeEM.vhd
@@ -24,6 +24,31 @@ ENTITY pipeEM IS
 END pipeEM;
 
 ARCHITECTURE arch_pipe of pipeEM IS
+SIGNAL WriteData_temp: STD_LOGIC_VECTOR(31 DOWNTO 0) := (others=>'0');
+SIGNAL ALU_temp: STD_LOGIC_VECTOR(31 DOWNTO 0) := (others=>'0');
+SIGNAL RegWrite_temp: STD_LOGIC := 0;
+SIGNAL MemtoReg_temp: STD_LOGIC := 0;
+SIGNAL WriteReg_temp: STD_LOGIC := 0;
+SIGNAL MemWrite_temp: STD_LOGIC := 0;
 BEGIN
-
+	PROCESS(clk, stallD)
+	BEGIN
+		--Read stored value--
+		WriteDataM <= WriteData_temp;
+		ALUOutM <= ALU_temp;
+		RegWriteM <= RegWrite_temp;
+		MemtoRegM <= MemtoReg_temp;
+		WriteRegM <= WriteReg_temp;
+		MemWriteM <= MemWrite_temp;
+		
+		--Store value--
+		IF clk'EVENT AND clk = '1' THEN
+			WriteData_temp <= WriteDataE;
+			ALU_temp <= ALUInE;
+			RegWrite_temp <= RegWriteE;
+			MemtoReg_temp <= MemtoRegE;
+			WriteReg_temp <= WriteRegE;
+			MemWrite_temp <= MemWriteE;
+		END IF;
+	END PROCESS;
 END arch_pipe;
